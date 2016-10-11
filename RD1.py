cf998cdf02eae08520ca0ed1c4f86529b298adf3
Neville Linden <nlinden@chadwickschool.org>
2016-10-10 23:50:56 -0700

    Added triangle function and combined both functions into an encompassing function.

--- RD1.py
+++ RD1.py
@@ -1,8 +1,23 @@
 from Myro import *
 init ("sim")
+def squareDraw():
+    penDown()
+    for x in range (0,4):
+        forward(2,2)
+        turnBy(90)
+    penUp()
+    
+def triangleDraw():
+    penDown()
+    for x in range (0,3):
+        forward (2,2)
+        turnBy(120)
+    penUp()
 
-penDown()
-for x in range (0,4):
-    forward(2,2)
+def someShapes():        
+    squareDraw()
     turnBy(90)
-penUp()
+    backward(1,2)
+    triangleDraw()
+
+someShapes()
